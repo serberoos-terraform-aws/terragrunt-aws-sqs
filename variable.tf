b260a10e89562ff48362c7c644c132ae649afae4
serberoos <jae99c@gmail.com>
2025-07-30 15:24:44 +0900

    batch: 모듈 표준화 개선 완료 - 2025-07-30 15:19:36

--- variable.tf
+++ variable.tf
@@ -1,23 +1,19 @@
 variable "sqs_queue_create" {
-  description = "Defined sqs_queue_create Information"
-  type        = map(any)
-  default     = {}
+  type    = map(any)
+  default = {}
 }
 
 variable "sqs_queue_policy_create" {
-  description = "Defined sqs_queue_policy_create Information"
-  type        = map(any)
-  default     = {}
+  type    = map(any)
+  default = {}
 }
 
 variable "sqs_queue_redrive_allow_policy_create" {
-  description = "Defined sqs_queue_redrive_allow_policy_create Information"
-  type        = map(any)
-  default     = {}
+  type    = map(any)
+  default = {}
 }
 
 variable "sqs_queue_redrive_policy_create" {
-  description = "Defined sqs_queue_redrive_policy_create Information"
-  type        = map(any)
-  default     = {}
+  type    = map(any)
+  default = {}
 } 
